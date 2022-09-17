459d094d60457cd6ce13f0674c4fb69a6065ad63
thiago.teixeira <thiago.teixeira@multilaser.com.br>
2022-09-17 14:30:31 -0300

    fix: fixes sln and folder name

---
+++ Fanout/Consumer/Fanout-consumer/Program.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Fanout_consumer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                var channel = connection.CreateModel();
+
+                var queueName = args[0];
+                channel.QueueDeclare(queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                BuildAndRunWorker(channel, "Worker A", queueName);
+                BuildAndRunWorker(channel, "Worker B", queueName);
+
+                Console.ReadLine();
+            }
+        }
+
+        public static void BuildAndRunWorker(IModel channel, string workerName, string queueName)
+        {
+            channel.BasicQos(0, 7, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                try
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body.ToArray());
+                    Console.WriteLine($"{channel.ChannelNumber} - {workerName}: [x] Received: {message}");
+
+                    channel.BasicAck(ea.DeliveryTag, false);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            };
+
+            channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+        }
+    }
+}
---
+++ Fanout/Producer/Fanout-producer/Program.cs
@@ -0,0 +1,83 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fanout_producer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            var manualResetEvent = new ManualResetEvent(initialState: false);
+
+            manualResetEvent.Reset();
+
+            using (var connection = factory.CreateConnection())
+            {
+                var queueName = "order";
+
+                var channel = SetupChannel(connection);
+
+                BuildAndRunPublishers(channel, queueName, "Produtor A", manualResetEvent);
+
+                manualResetEvent.WaitOne();
+            }
+
+        }
+
+        public static IModel SetupChannel(IConnection connection)
+        {
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "order", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            channel.QueueDeclare(queue: "logs", durable: false, exclusive: false, autoDelete: false, arguments: null);
+            channel.QueueDeclare(queue: "finance_orders", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+            channel.ExchangeDeclare("order", type: "fanout");
+
+            channel.QueueBind("order", "order", ""); //Exchange do tipo Fanout ignora o RoutingKey, mais usado no tipo Direct
+            channel.QueueBind("logs", "order", "");
+            channel.QueueBind("finance_orders", "order", "");
+
+            return channel;
+        }
+
+        public static void BuildAndRunPublishers(IModel chanel, string queueName, string publisherName, ManualResetEvent manualResetEvent)
+        {
+            Task.Run(() =>
+            {
+                int count = 0;
+                while (true)
+                {
+                    try
+                    {
+                        Console.WriteLine("Pressione qualquer tecla para produzir 10 mensagens.");
+                        Console.ReadLine();
+
+                        for (int i = 0; i < 10; i++)
+                        {
+                            var message = $"OrderNumber: {count++} from {publisherName})";
+                            var body = Encoding.UTF8.GetBytes(message);
+
+                            chanel.BasicPublish(exchange: "order", "", null, body);
+
+                            Console.WriteLine($"{publisherName} - [x] Sent {count}", message);
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+
+                        manualResetEvent.Set();
+                    }
+                }
+            });
+        }
+
+    }
+}
